45d9b7777d116bddd1cd7266d3a161f2da54a28e
backendgitlogix123 <101447820+backendgitlogix123@users.noreply.github.com>
2022-03-21 22:49:24 +0500

    api endpoint is added on existing function

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "Hello World to me!"
+    return "API health is fine!"
 
+@app.route('/api-health')
 @app.route('/health')
 def health_check():
     return "API health is fine!"
